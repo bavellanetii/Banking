2e1da68efdf4925117f67efd18e4bcb97f6cea3b
bavellanetii <78850672+bavellanetii@users.noreply.github.com>
2021-12-08 11:33:29 +0000

    Linked Form2 to the SQL database and created labels, textboxes, and a button which submits new user data to the database

--- Form2.Designer.cs
+++ Form2.Designer.cs
@@ -29,12 +29,100 @@ namespace Banking
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelPin = new System.Windows.Forms.Label();
+            this.labelBalance = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxPin = new System.Windows.Forms.TextBox();
+            this.textBoxBalance = new System.Windows.Forms.TextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(101, 88);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(42, 15);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Name:";
+            // 
+            // labelPin
+            // 
+            this.labelPin.AutoSize = true;
+            this.labelPin.Location = new System.Drawing.Point(101, 152);
+            this.labelPin.Name = "labelPin";
+            this.labelPin.Size = new System.Drawing.Size(27, 15);
+            this.labelPin.TabIndex = 1;
+            this.labelPin.Text = "Pin:";
+            // 
+            // labelBalance
+            // 
+            this.labelBalance.AutoSize = true;
+            this.labelBalance.Location = new System.Drawing.Point(101, 209);
+            this.labelBalance.Name = "labelBalance";
+            this.labelBalance.Size = new System.Drawing.Size(51, 15);
+            this.labelBalance.TabIndex = 2;
+            this.labelBalance.Text = "Balance:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(158, 88);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 23);
+            this.textBoxName.TabIndex = 3;
+            // 
+            // textBoxPin
+            // 
+            this.textBoxPin.Location = new System.Drawing.Point(158, 144);
+            this.textBoxPin.Name = "textBoxPin";
+            this.textBoxPin.Size = new System.Drawing.Size(100, 23);
+            this.textBoxPin.TabIndex = 4;
+            // 
+            // textBoxBalance
+            // 
+            this.textBoxBalance.Location = new System.Drawing.Point(158, 206);
+            this.textBoxBalance.Name = "textBoxBalance";
+            this.textBoxBalance.Size = new System.Drawing.Size(100, 23);
+            this.textBoxBalance.TabIndex = 5;
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(158, 304);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(100, 23);
+            this.buttonSubmit.TabIndex = 6;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(458, 448);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.textBoxBalance);
+            this.Controls.Add(this.textBoxPin);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.labelBalance);
+            this.Controls.Add(this.labelPin);
+            this.Controls.Add(this.labelName);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelPin;
+        private System.Windows.Forms.Label labelBalance;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxPin;
+        private System.Windows.Forms.TextBox textBoxBalance;
+        private System.Windows.Forms.Button buttonSubmit;
     }
 }
--- Form2.cs
+++ Form2.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SqlClient;
+
 namespace Banking
 {
     public partial class Form2 : Form
@@ -18,6 +20,21 @@ namespace Banking
         {
             InitializeComponent();
             instance = this;
+        }
+
+        SqlConnection con1 = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\bavel\source\repos\Banking\Database1.mdf;Integrated Security=True");
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            SqlCommand sm = new SqlCommand("insert into username values('"+textBoxName.Text+"','"+textBoxPin.Text+"','"+textBoxBalance.Text+"')",con1);
+
+            con1.Open();
+
+            sm.ExecuteNonQuery();
+
+            con1.Close();
+
+            MessageBox.Show("Data Saved Successfully");
         }
     }
 }
